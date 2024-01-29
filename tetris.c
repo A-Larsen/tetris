d00b0a6e94848a7f5aa25742aa76c087cf50ebc5
Austin Larsen <austin.larsen@mailfence.com>
2024-01-29 09:05:27 -0500

    refactoring

--- tetris.c
+++ tetris.c
@@ -3,115 +3,104 @@
 #include <SDL2/SDL.h>
 #include <math.h>
 
-#define ARENA_WIDTH 400U
-#define ARENA_HEIGHT 800U
-
-typedef struct _Tetrominos {
-    const uint8_t I[8];
-    const uint8_t J[8];
-    const uint8_t L[8];
-    const uint8_t O[8];
-    const uint8_t Z[8];
-    const uint8_t S[8];
-    const uint8_t T[8];
-    const uint8_t size;
-} Tetrominos;
-
+#define SCREEN_WIDTH_PX 400U
+#define SCREEN_HEIGHT_PX 800U
+#define ARENA_WIDTH 8
+#define ARENA_HEIGHT 16
+#define PEICE_WIDTH 4
+#define PEICE_HEIGHT 4
 #define TETROMINOS_DATA_SIZE 8
+#define GET_PLACED_POSITION(pos) pos.y * ARENA_WIDTH + pos.x;
+#define PEICE_I 0
+#define PEICE_J 1
+#define PEICE_L 2
+#define PEICE_O 3
+#define PEICE_S 4
+#define PEICE_T 5
+#define PEICE_Z 6
+#define PEICE_SIZE 50
+static uint8_t placed[128]; // 16 x 8
 
-enum TET_TYPES {
-    TET_I,
-    TET_J,
-    TET_L,
-    TET_O,
-    TET_S,
-    TET_T,
-    TET_Z,
-};
-Tetrominos tetrominos = {
+uint8_t tetris_tetrominos[7][8] = {
+    [PEICE_I] = {0,0,0,0,
+                 1,1,1,1},
 
-    .I = {0,0,0,0,
-          1,1,1,1},
+    [PEICE_J] = {1,0,0,0,
+                 1,1,1,0},
 
-    .J = {1,0,0,0,
-          1,1,1,0},
+    [PEICE_L] = {0,0,1,0,
+                 1,1,1,0},
 
-    .L = {0,0,1,0,
-          1,1,1,0},
-
-    .O = {1,1,0,0,
+    [PEICE_O] = {1,1,0,0,
           1,1,0,0},
 
-    .S = {0,1,1,0,
+    [PEICE_S] = {0,1,1,0,
           1,1,0,0},
 
-    .T = {0,1,0,0,
+    [PEICE_T] = {0,1,0,0,
           1,1,1,0},
 
-    .Z = {1,1,0,0,
+    [PEICE_Z] = {1,1,0,0,
           0,1,1,0},
 
-    .size = 50
 };
 
-uint8_t placed[128]; // 16 x 8
-
-void drawTetromino(SDL_Renderer *renderer, const uint8_t peice[8],
+void tetris_drawTetromino(SDL_Renderer *renderer, uint8_t peice,
                    SDL_Point position)
 {
     for (int i = 0; i < TETROMINOS_DATA_SIZE; ++i) {
-        if (!peice[i]) continue;
-        uint8_t x = i % 4;
-        uint8_t y = floor((float)i / 4);
+        if (!tetris_tetrominos[peice][i]) continue;
+        uint8_t x = i % PEICE_WIDTH;
+        uint8_t y = floor((float)i / PEICE_HEIGHT);
         SDL_Rect rect = {
-            .x = (x + position.x) * tetrominos.size, .y = (y + position.y) * 
-                 tetrominos.size,
-            .w = tetrominos.size, .h = tetrominos.size
+            .x = (x + position.x) * PEICE_SIZE, .y = (y + position.y) * 
+                 PEICE_SIZE,
+            .w = PEICE_SIZE, .h = PEICE_SIZE
         };
         SDL_RenderFillRect(renderer, &rect);
     }
 }
 
-void addToPlaced(const uint8_t peice[8], SDL_Point position) {
+void tetris_addToPlaced(uint8_t peice, SDL_Point position) {
 
-    uint8_t pos = position.y * 8 + position.x;
-    for (int i = 0; i < 4; ++i) {
-        if (peice[i]) {
+    uint8_t pos = GET_PLACED_POSITION(position);
+    for (int i = 0; i < PEICE_WIDTH; ++i) {
+        if (tetris_tetrominos[peice][i]) {
             placed[pos + i] = 1;
         }
-        if (peice[i + 4]) {
-            placed[pos + 8 + i] = 1;
+        if (tetris_tetrominos[peice][i + PEICE_WIDTH]) {
+            placed[pos + ARENA_WIDTH + i] = 1;
         }
     }
 }
 
-void printPlaced() {
+void tetris_printPlaced() {
     uint8_t i = 0;
 
-    for (int y = 0; y < 16; ++y) {
-        printf("%.3d: ", y * 8);
-        for (int x = 0; x < 8; ++x) {
-            i = (y * 8) + x;
+    for (int y = 0; y < ARENA_HEIGHT; ++y) {
+        printf("%.3d: ", y * ARENA_WIDTH);
+        for (int x = 0; x < ARENA_WIDTH; ++x) {
+            i = (y * ARENA_WIDTH) + x;
             printf("%d", placed[i]);
         }
         printf("\n");
     }
 }
 
-void checkIfFits() {
+bool tetris_check(SDL_Point position) {
+    uint8_t pos = GET_PLACED_POSITION(position);
+    return false;
 }
 
 int main(void)
 {
     memset(&placed, 0, sizeof(uint8_t) * 128);
-    /* placed[14] = 1; */
-    /* printPlaced(); */
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
         fprintf(stderr, "could not initialize SDL2\n");
         exit(1);
     }
     SDL_Window *window = SDL_CreateWindow("tetris", SDL_WINDOWPOS_UNDEFINED, 
-                     SDL_WINDOWPOS_UNDEFINED, ARENA_WIDTH, ARENA_HEIGHT,
+                     SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH_PX, SCREEN_HEIGHT_PX,
                      SDL_WINDOW_SHOWN);
 
     SDL_Renderer *renderer = SDL_CreateRenderer(window, 0, 
@@ -121,8 +110,7 @@ int main(void)
     bool quit = false;
 
     bool doOnce = true;
-    uint8_t *peice; 
-    memcpy(peice, &tetrominos.T, sizeof(uint8_t) * 8);
+    uint8_t peice = PEICE_T;
     SDL_Point point = {.x = 0, .y = 0};
 
     int i = 0;
@@ -134,7 +122,7 @@ int main(void)
         SDL_RenderClear(renderer);
 
         SDL_SetRenderDrawColor(renderer, 255, 0, 0, 0);
-        drawTetromino(renderer, peice, point);
+        tetris_drawTetromino(renderer, peice, point);
 
         while(SDL_PollEvent(&event)) {
 
@@ -172,12 +160,12 @@ int main(void)
         }
 
         if (i % fall_speed == 0) {
-            if (point.y + 2 < ARENA_HEIGHT / tetrominos.size)  {
+            if (point.y + 2 < SCREEN_HEIGHT_PX / PEICE_SIZE)  {
                 point.y++;
             } else if (doOnce) {
                 printf("add to placed\n\n");
-                addToPlaced(peice, point);
-                printPlaced();
+                tetris_addToPlaced(peice, point);
+                tetris_printPlaced();
                 doOnce = false;
             }
         }
